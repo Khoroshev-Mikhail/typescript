778b39691f66b47e10b0c9cf7b32d5cc7ad68e92
Mikha38 <oyks1379@yandex.ru>
2022-07-22 23:16:35 +0300

    home

--- ts/src/domain/Person.ts
+++ ts/src/domain/Person.ts
@@ -1,12 +1,8 @@
 import { Identifiable } from "./Identifiable";
 
-type Sex =
-  | "male"
-  | "female";
- 
 export interface Person extends Identifiable {
   name: string,
   surname: string | null,
   age?: number,
-  sex: Sex,
+  sex: 'male' | 'female',
 }
